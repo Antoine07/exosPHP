e37ee98b78c570c6e26e21b308258f7415d002bd
mathieuCstn <mathieuconstantin01@gmail.com>
2023-12-13 10:23:41 +0100

    ex9

--- corrections/ex9.php
+++ corrections/ex9.php
@@ -5,3 +5,4 @@ $alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 $phrase = "BONJOUR"; 
 
 // Créé une fonction crypto qui décalle chaque lettre de +2 par rapport à l'alphabet.
+// Vous avez la fonction strlen() pour récupérer la longueur d'une chaîne de charactère.
